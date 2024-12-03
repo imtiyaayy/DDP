fbb7b39abe5ec75b5a3f12207d4bb7d5c4580fc4
imtiyaayy <imtiyazmufidah08@gmail.com>
2024-12-03 21:07:18 +0700

    Add files via upload

---
+++ call.py
@@ -0,0 +1,26 @@
+import bangundatar
+
+bangundatar.l_persegi(6)
+bangundatar.l_persegi_panjang(12,8)
+bangundatar.l_segitiga(3,8)
+bangundatar.l_lingkaran(1)
+
+print()
+
+import bangunruang
+
+bangunruang.l_balok(2,4,6)
+bangunruang.l_prismasegitiga(3,9,5)
+bangunruang.l_limas(9,5)
+bangunruang.l_tabung(2,6)
+bangunruang.l_kubus(4)
+
+print()
+
+import operasimatematika
+
+operasimatematika.tambah(8,3)
+operasimatematika.kurang(22,6)
+operasimatematika.kali(4,2)
+operasimatematika.bagi(12,4)
+operasimatematika.pangkat(4,3)
